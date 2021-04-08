3f3b4d553dca7155f3f3d7ac9d597fa57b24ce0a
hg5901725@gmail.com <hg5901725@gmail.com>
2021-04-08 10:48:59 -0600

    Diseño de la ventada menu principal

---
+++ Clinica-Dental/menuPrincipal.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Clinica_Dental
+{
+    /// <summary>
+    /// Lógica de interacción para menuPrincipal.xaml
+    /// </summary>
+    public partial class menuPrincipal : Window
+    {
+        DispatcherTimer Timer;
+        double panelWidth;
+        bool hidden;
+        public menuPrincipal()
+        {
+            InitializeComponent();
+            Timer = new DispatcherTimer();
+            Timer.Interval = new TimeSpan(0, 0, 0, 0, 10);
+
+            panelWidth = SidePanel.Width;
+        }
+        
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if (hidden)
+            {
+                SidePanel.Width += 1;
+                if (SidePanel.Width >= panelWidth)
+                {
+                    Timer.Stop();
+                    hidden = false;
+                }
+            }
+            else
+            {
+                SidePanel.Width -= 1;
+                if (SidePanel.Width <= 30)
+                {
+                    Timer.Stop();
+                    hidden = true;
+                }
+            }
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Timer.Start();
+            Application.Current.Shutdown();
+        }
+        private void PanelHeader_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+    }
+}
