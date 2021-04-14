aed3ab38eb8829d151981f73ba1b73db7f08390e
jordimartinezcarbajal14@gmail.com <jordimartinezcarbajal14@gmail.com>
2021-04-14 16:23:37 -0600

    Creacion de la iterfaz de Historiales Clinicos

---
+++ Clinica-Dental/historialClinico.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Clinica_Dental
+{
+    /// <summary>
+    /// Lógica de interacción para historialClinico.xaml
+    /// </summary>
+    public partial class historialClinico : Window
+    {
+        public historialClinico()
+        {
+            InitializeComponent();
+        }
+    }
+}
