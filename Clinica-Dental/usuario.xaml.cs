e7f405c228ce186c15699a5d44afa5973347f5dc
Mario Martinez <mariojmartinez12@gmail.com>
2021-04-15 01:46:22 -0500

    Codificacion de xaml.cs del usuario y correcciones a la interfaz

--- Clinica-Dental/usuario.xaml.cs
+++ Clinica-Dental/usuario.xaml.cs
@@ -19,9 +19,109 @@ namespace Clinica_Dental
     /// </summary>
     public partial class usuario : Window
     {
+        private Usuarios usuarios = new Usuarios();
+        private List<Usuarios> username;
         public usuario()
         {
             InitializeComponent();
+            cmbEstado.ItemsSource = Enum.GetValues(typeof(EstadoUsuario));
+            cmbPuesto.ItemsSource = Enum.GetValues(typeof(TipoUsuario));
+            ObtenerUsuario();
+        }
+
+        private void ObtenerUsuario()
+        {
+            username = usuarios.MostrarUsuario();
+            dgvUsuario.ItemsSource = username;
+        }
+
+        private void LimpiarFormulario()
+        {
+            txtContraseña.Text = string.Empty;
+            txtNombre.Text = string.Empty;
+            txtUsuario.Text = string.Empty;
+            cmbEstado.SelectedValue = null;
+            cmbPuesto.SelectedValue = null;
+        }
+
+        private void ObtenerValoresFormulario()
+        {
+            usuarios.NombreCompleto = txtNombre.Text;
+            usuarios.Username = txtUsuario.Text;
+            usuarios.Password = txtContraseña.Text;
+            usuarios.EstadoUsuario = (EstadoUsuario)cmbEstado.SelectedValue;
+            usuarios.TipoUsuario = (TipoUsuario)cmbPuesto.SelectedValue;
+        }
+        private void ObtenerValoresDesdeObjeto()
+        {
+            txtNombre.Text = usuarios.NombreCompleto;
+            txtUsuario.Text = usuarios.Username;
+            txtContraseña.Text = usuarios.Password;
+            cmbEstado.SelectedValue = usuarios.EstadoUsuario;
+            cmbPuesto.SelectedValue = usuarios.TipoUsuario;
+        }
+
+        private bool VerificarDatos()
+        {
+            if (txtContraseña.Text == string.Empty || txtNombre.Text == string.Empty || txtUsuario.Text == string.Empty)
+            {
+                MessageBox.Show("Por favor ingresa todos los valores en las cajas de texto");
+                return false;
+            }
+            else if (cmbEstado.SelectedValue == null)
+            {
+                MessageBox.Show("Por favor selecciona el estado del usuario");
+                return false;
+            }
+            else if (cmbPuesto.SelectedValue == null)
+            {
+                MessageBox.Show("Por favor selecciona el tipo de usuario");
+                return false;
+            }
+            return true;
+        }
+        private void btnAgregar_Click(object sender, RoutedEventArgs e)
+        {
+            
+            if (VerificarDatos())
+            {
+                try
+                {
+                   
+                    ObtenerValoresFormulario();
+
+                    
+                    usuarios.AgregarUsuario(usuarios);
+
+                    
+                    MessageBox.Show("¡Datos insertados correctamente!");
+
+                }
+                catch (Exception ex)
+                {
+                    
+                    throw ex;
+                }
+                finally
+                {
+                    LimpiarFormulario();
+                    ObtenerUsuario();
+                }
+            }
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+        private void dgvUsuario_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            Usuarios usuarioSeleccionado = dgvUsuario.SelectedItem as Usuarios;
+            usuarios = usuarios.BuscarUsuario(usuarioSeleccionado.Id);
+
+            ObtenerValoresDesdeObjeto();
+           
         }
     }
 }
