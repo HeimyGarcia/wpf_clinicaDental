2cb44a89c0286e09f9ae5251204117e0e473222e
hg5901725@gmail.com <hg5901725@gmail.com>
2021-04-15 00:26:32 -0600

    Creacion de la clase, ventana y funcionalidad CRUD de la tabla "ConsultaTratamiento"

---
+++ Clinica-Dental/ConsultaTratamiento.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Agregar los namespaces de conexión con SQL Server
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Clinica_Dental
+{
+    public enum estadoConsultaTratamiento
+    {
+        Activo = 1,
+        Inactivo = 0
+    }
+    class ConsultaTratamiento
+    {
+        // Variables miembro
+        private static string connectionString = ConfigurationManager.ConnectionStrings["Clinica_Dental.Properties.Settings.ClinicaDentalConnectionString"].ConnectionString;
+        private SqlConnection sqlConnection = new SqlConnection(connectionString);
+
+        // Propiedades
+        public int IdTratamiento { get; set; }
+
+        public int IdHistorialConsulta { get; set; }
+
+        public estadoConsultaTratamiento Estado { get; set; }
+
+        // Constructores
+        public ConsultaTratamiento() { }
+
+        public ConsultaTratamiento(int idTratamiento, int idHistorialConsulta, estadoConsultaTratamiento estado)
+        {
+            IdTratamiento = idTratamiento;
+            IdHistorialConsulta = idHistorialConsulta;
+            Estado = estado;
+        }
+        private int ObtenerEstado(estadoConsultaTratamiento estado)
+        {
+            switch (estado)
+            {
+                case estadoConsultaTratamiento.Activo:
+                    return 1;
+                case estadoConsultaTratamiento.Inactivo:
+                    return 0;
+                default:
+                    return 1;
+            }
+        }
+        private int CambiarEstado(estadoConsultaTratamiento estado)
+        {
+            switch (estado)
+            {
+                case estadoConsultaTratamiento.Activo:
+                    return 0;
+                case estadoConsultaTratamiento.Inactivo:
+                    return 0;
+                default:
+                    return 0;
+            }
+        }
+
+        //Metodos
+        /// <summary>
+        /// Inserta una Tratamiento.
+        /// </summary>
+        /// <param name="tratamiento">La información del tratamiento</param>
+        public void CrearTratamiento(ConsultaTratamiento tratamiento)
+        {
+            try
+            {
+                // Query de inserción
+                string query = @"INSERT INTO Pacientes.ConsultaTratamiento (idTratamiento, idHistorialConsulta, estado)
+                                 VALUES (@idTratamiento, @idHistorialConsulta,@estado)";
+
+                // Establecer la conexión
+                sqlConnection.Open();
+
+                // Crear el comando SQL
+                SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+
+                // Establecer los valores de los parámetros
+                sqlCommand.Parameters.AddWithValue("@idTratamiento", tratamiento.IdTratamiento);
+                sqlCommand.Parameters.AddWithValue("@idHistorialConsulta", tratamiento.IdHistorialConsulta);
+                sqlCommand.Parameters.AddWithValue("@estado", ObtenerEstado(tratamiento.Estado));
+
+                // Ejecutar el comando de inserción
+                sqlCommand.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                // Cerrar la conexión
+                sqlConnection.Close();
+            }
+        }
+
+        /// <summary>
+        /// Muestra todos los Tratamiento
+        /// </summary>
+        /// <returns>Un listado de los Tratamiento</returns>
+        public List<ConsultaTratamiento> MostrarTratamiento()
+        {
+            // Inicializar una lista vacía de tratamientos
+            List<ConsultaTratamiento> tratamientos = new List<ConsultaTratamiento>();
+
+            try
+            {
+                // Query de selección
+                string query = @"SELECT * FROM Pacientes.ConsultaTratamiento";
+
+                // Establecer la conexión
+                sqlConnection.Open();
+
+                // Crear el comando SQL
+                SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+
+                // Obtener los datos de las habitaciones
+                using (SqlDataReader rdr = sqlCommand.ExecuteReader())
+                {
+                    while (rdr.Read())
+                    {
+                        tratamientos.Add(new ConsultaTratamiento
+                        {
+                            IdTratamiento = Convert.ToInt32(rdr["idTratamiento"]),
+                            IdHistorialConsulta = Convert.ToInt32(rdr["idHistorialConsulta"]),
+                            Estado = (estadoConsultaTratamiento)Convert.ToInt32((rdr["estado"])) //probar
+                        });
+                    }
+                }
+                return tratamientos;
+
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                // Cerrar la conexión
+                sqlConnection.Close();
+            }
+        }
+
+        /// <summary>
+        /// Obtiene un detalle por su Tratamiento
+        /// </summary>
+        /// <param name="idTratamiento">El idTratamiento del DetalleTratamiento</param>
+        /// <param name="idHistorialConsulta">El idHistorialConsulta del Tratamiento</param>
+        /// <returns>Los datos del DetalleTratamiento</returns>
+        public ConsultaTratamiento BuscarTratamiento(int idTratamiento, int idHistorialConsulta)
+        {
+            ConsultaTratamiento elTratamiento = new ConsultaTratamiento();
+
+            try
+            {
+                // Query de búsqueda
+                string query = @"SELECT * FROM Pacientes.ConsultaTratamiento
+                                 WHERE idTratamiento = @idTratamiento and idHistorialConsulta=@idHistorialConsulta";
+
+                // Establecer la conexión
+                sqlConnection.Open();
+
+                // Crear el comando SQL
+                SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+
+                // Establecer el valor del parámetro
+                sqlCommand.Parameters.AddWithValue("@idTratamiento", idTratamiento);
+                sqlCommand.Parameters.AddWithValue("@idHistorialConsulta", idHistorialConsulta);
+                using (SqlDataReader rdr = sqlCommand.ExecuteReader())
+                {
+                    while (rdr.Read())
+                    {
+                        elTratamiento.IdTratamiento = Convert.ToInt32(rdr["idTratamiento"]);
+                        elTratamiento.IdTratamiento = Convert.ToInt32(rdr["idHistorialConsulta"]);
+                        elTratamiento.Estado = (estadoConsultaTratamiento)Convert.ToInt32((rdr["estado"])); //probar
+                    }
+                }
+
+                return elTratamiento;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                // Cerrar la conexión
+                sqlConnection.Close();
+            }
+        }
+
+        /// <summary>
+        /// Modifica los datos de Tratamiento
+        /// </summary>
+        /// <param name="tratamiento">Informacion de Tratamiento</param>
+        public void ModificarTratamiento(ConsultaTratamiento tratamiento)
+        {
+            try
+            {
+                // Query de actualización
+                string query = @"UPDATE Pacientes.ConsultaTratamiento
+                                 SET idTratamiento = @idTratamiento, idHistorialConsulta = @idHistorialConsulta, estado = @estado
+                                 WHERE idTratamiento = @idTratamiento and idHistorialConsulta=@idHistorialConsulta";
+
+                // Establecer la conexión
+                sqlConnection.Open();
+
+                // Crear el comando SQL
+                SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+
+                // Establecer los valores de los parámetros
+                sqlCommand.Parameters.AddWithValue("@idTratamiento", tratamiento.IdTratamiento);
+                sqlCommand.Parameters.AddWithValue("@idHistorialConsulta", tratamiento.IdHistorialConsulta);
+                sqlCommand.Parameters.AddWithValue("@estado", ObtenerEstado(tratamiento.Estado));
+
+                // Ejecutar el comando de actualización
+                sqlCommand.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                // Cerrar la conexión
+                sqlConnection.Close();
+            }
+        }
+
+
+        /// <summary>
+        /// Elimina un Tratamiento
+        /// </summary>
+        /// <param name="tratamiento">La informacion de tratamiento</param>
+        public void EliminarTratamiento(ConsultaTratamiento tratamiento)
+        {
+            try
+            {
+                // Query de actualización
+                string query = @"UPDATE Pacientes.ConsultaTratamiento
+                                 SET estado = @estado
+                                 WHERE idTratamiento = @idTratamiento and idHistorialConsulta=@idHistorialConsulta";
+
+                // Establecer la conexión
+                sqlConnection.Open();
+
+                // Crear el comando SQL
+                SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+
+                // Establecer los valores de los parámetros
+
+                sqlCommand.Parameters.AddWithValue("@estado", CambiarEstado(tratamiento.Estado));
+                sqlCommand.Parameters.AddWithValue("@idTratamiento", tratamiento.IdTratamiento);
+                sqlCommand.Parameters.AddWithValue("@idHistorialConsulta", tratamiento.IdHistorialConsulta);
+
+                // Ejecutar el comando de actualización
+                sqlCommand.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                // Cerrar la conexión
+                sqlConnection.Close();
+            }
+        }
+    }
+}
---
+++ Clinica-Dental/consultaTratamiento.xaml.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+// Agregar los namespaces de conexión con SQL Server
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Globalization;
+
+namespace Clinica_Dental
+{
+    /// <summary>
+    /// Lógica de interacción para consultaTratamiento.xaml
+    /// </summary>
+    public partial class consultaTratamiento : Window
+    {
+        private static string connectionString = ConfigurationManager.ConnectionStrings["Clinica_Dental.Properties.Settings.ClinicaDentalConnectionString"].ConnectionString;
+        private SqlConnection sqlConnection = new SqlConnection(connectionString);
+
+        // Variables miembro
+        private ConsultaTratamiento elTratamiento = new ConsultaTratamiento();
+        private List<ConsultaTratamiento> tratamientos;
+
+        //informacion de la llaves foraneas
+
+        private DetalleTratamiento detalleTratamiento = new DetalleTratamiento();
+        private List<DetalleTratamiento> detalleTratamientos = new List<DetalleTratamiento>();
+        public consultaTratamiento()
+        {
+            InitializeComponent();
+            // Llenar el combobox de estado
+            cmbEstado.ItemsSource = Enum.GetValues(typeof(estadoConsultaTratamiento));
+            // Llenar el listbox de Tratamiento
+            ObtenerTratamiento();
+            //Llenar el combobox de detalleTratamiento
+            ObtenerListaDetalleTratamiento();
+        }
+
+        private void ObtenerListaDetalleTratamiento()
+        {
+            SqlCommand command = new SqlCommand("Select nombreTratamiento from Pacientes.DetalleTratamiento", sqlConnection);
+            sqlConnection.Open();
+            SqlDataReader rdr = command.ExecuteReader();
+            while (rdr.Read())
+            {
+                cmbTratamiento.Items.Add(rdr["nombreTratamiento"].ToString());
+
+            }
+            sqlConnection.Close();
+
+
+        }
+        private void LimpiarFormulario()
+        {
+            cmbEstado.Text = null;
+            cmbTratamiento.Text = null;
+            txtConsulta.Text = string.Empty;
+        }
+        private void ObtenerTratamiento()
+        {
+            tratamientos = elTratamiento.MostrarTratamiento();
+            dgvTratamiento.ItemsSource = tratamientos;
+
+        }
+
+        private void ObtenerValoresFormulario()
+        {
+            elTratamiento.IdTratamiento = Convert.ToInt32(cmbTratamiento.Text);
+            elTratamiento.IdHistorialConsulta = Convert.ToInt32(txtConsulta.Text);
+            elTratamiento.Estado = (estadoConsultaTratamiento)cmbEstado.SelectedItem;
+        }
+        private bool VerificarValores()
+        {
+            if (cmbEstado.SelectedValue == null)
+            {
+                MessageBox.Show("Por favor selecciona el estado del DetalleTratamiento");
+                return false;
+            }
+            else if (cmbTratamiento.SelectedValue == null)
+            {
+                MessageBox.Show("Por favor selecciona el tratamiento");
+                return false;
+            }
+            else if (txtConsulta.Text == string.Empty)
+            {
+                MessageBox.Show("Por favor selecciona la consulta");
+                return false;
+            }
+
+            return true;
+        }
+        private void Inhabilitar()
+        {
+            btnAgregar.IsEnabled = false;
+        }
+        private void Habilitar()
+        {
+            btnAgregar.IsEnabled = true;
+        }
+        private void ValoresFormularioDesdeObjeto()
+        {
+            cmbTratamiento.Text = Convert.ToInt32(elTratamiento.IdTratamiento).ToString();
+            txtConsulta.Text = Convert.ToInt32(elTratamiento.IdHistorialConsulta).ToString();
+            cmbEstado.SelectedValue = elTratamiento.Estado;
+
+        }
+        private void btnAgregar_Click(object sender, RoutedEventArgs e)
+        {
+            // Verificar que se ingresaron los valores requeridos
+            if (VerificarValores())
+            {
+                try
+                {
+                    // Obtener los valores para la habitación
+                    ObtenerValoresFormulario();
+
+                    // Insertar los datos de la habitación
+                    elTratamiento.CrearTratamiento(elTratamiento);
+
+                    // Mensaje de inserción exitosa
+                    MessageBox.Show("¡Datos insertados correctamente!");
+
+                }
+                catch (Exception ex)
+                {
+                    //MessageBox.Show("Ha ocurrido un error al momento de insertar al paciente...");
+                    //Console.WriteLine(ex.Message)
+                    throw ex;
+                }
+                finally
+                {
+                    LimpiarFormulario();
+                    ObtenerTratamiento();
+                }
+            }
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void dgvDetalleTratamiento_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            ConsultaTratamiento tratamientoSelecionado = dgvTratamiento.SelectedItem as ConsultaTratamiento;
+            elTratamiento = elTratamiento.BuscarTratamiento(tratamientoSelecionado.IdHistorialConsulta, tratamientoSelecionado.IdTratamiento);
+
+            ValoresFormularioDesdeObjeto();
+            Inhabilitar();
+        }
+
+        private void btnModificar_Click(object sender, RoutedEventArgs e)
+        {
+            if (dgvTratamiento.SelectedValue == null)
+                MessageBox.Show("Por favor selecciona un tratamiento desde el listado");
+            else
+            {
+                if (VerificarValores())
+                {
+                    try
+                    {
+                        // Obtener los valores para la consulta desde el formulario
+                        ObtenerValoresFormulario();
+
+                        // Mostrar un mensaje de confirmación
+                        MessageBoxResult result = MessageBox.Show("¿Deseas modificar el tratamiento?", "Confirmar", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+
+                        if (result == MessageBoxResult.Yes)
+                        {
+                            // Modificar Tratamiento
+                            elTratamiento.ModificarTratamiento(elTratamiento);
+
+                            // Mensaje de actualización realizada
+                            MessageBox.Show("¡Tratamiento modificada correctamente!");
+                        }
+
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Ha ocurrido un error al momento de modificar el tratamiento ...");
+                        MessageBox.Show(ex.Message);
+                    }
+                    finally
+                    {
+                        // Actualizar los pacientes
+                        ObtenerTratamiento();
+
+                        LimpiarFormulario();
+                        Habilitar();
+                    }
+                }
+            }
+        }
+
+        private void btnEliminar_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (dgvTratamiento.SelectedValue == null)
+                    MessageBox.Show("Por favor selecciona un tratmiento desde el listado");
+                else
+                {
+                    // Mostrar un mensaje de confirmación
+                    MessageBoxResult result = MessageBox.Show("¿Deseas eliminar un tratamiento?", "Confirmar", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+
+                    if (result == MessageBoxResult.Yes)
+                    {
+                        // Eliminar la habitación
+                        elTratamiento.EliminarTratamiento(elTratamiento);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show("Ha ocurrido un error al momento de eliminar la consulta...");
+                //Console.WriteLine(ex.Message);
+                throw ex;
+            }
+            finally
+            {
+                // Actualizar los pacientes
+                ObtenerTratamiento();
+
+                LimpiarFormulario();
+                Habilitar();
+            }
+        }
+
+        private void btnBuscar_Click(object sender, RoutedEventArgs e)
+        {
+            elTratamiento = elTratamiento.BuscarTratamiento(Convert.ToInt32(txtTratamiento.Text), Convert.ToInt32(txtConsulta.Text));
+
+            ValoresFormularioDesdeObjeto();
+        }
+
+    }
+}
+
